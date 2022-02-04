53c6f21297ab01850198f3b81f255e8de21111d0
James Coulter <jamesdcoulter1972@gmail.com>
2022-02-04 11:31:10 -0800

    Add NFT art generation feature and resize of art

--- public/sketch.js
+++ public/sketch.js
@@ -1,31 +1,9 @@
-// var ww = window.innerWidth;
-// var hh = window.innerHeight;
-function genTokenData(projectNum) {
-  let data = {};
-  let hash = "0x";
-  for (var i = 0; i < 64; i++) {
-    hash += Math.floor(Math.random() * 16).toString(16);
-  }
-  data.hash = hash;
-  data.tokenId = (projectNum * 1000000 + mintNumber +1).toString();
-  return data;
-}
-let tokenData = genTokenData(787);
-let projectNumber = Math.floor(parseInt(tokenData.tokenId) / 1000000)
-let mintNumber = parseInt(tokenData.tokenId) % 1000000
+var sww = 700;
+var shh = 840;
 
-console.log(projectNumber);
-console.log(mintNumber);
-
-var ww = 700;
-var hh = 840;
-
-let canvas
-const st_deviation = 50
-const layers = 80
-
-let pentagon1;
-let pentagon2;
+let canvas;
+const st_deviation = 50;
+const layers = 80;
 
 var left_x;
 var right_x;
@@ -61,101 +39,65 @@ function createStretchedPentagon(stretchFactor, offset) {
 
 function preload() {
   img =loadImage('assets/bitmap'+random([1,2,3,4,5])+'.png');
-  
 }
 
 function setup() {
-  
-  pentagon1 = [
-      createVector(910, 320),
-      createVector(830.9188309203678, 510.9188309203678),
-      createVector(640, 590),
-      createVector(449.0811690796322, 510.91883092036784),
-      createVector(370 ,320.00000000000006),
-      createVector(449.0811690796321, 129.0811690796322),
-      createVector(640, 50),
-      createVector(830.9188309203678, 129.08116907963213)
-  ]
-
-  pentagon2 = [
-    createVector(910, 860),
-    createVector(830.9188309203678, 1050.9188309203678),
-    createVector(640, 1130),
-    createVector(449.0811690796322, 1050.91883092036784),
-    createVector(370 ,860.00000000000006),
-    createVector(449.0811690796321, 669.0811690796322),
-    createVector(640, 590),
-    createVector(830.9188309203678, 669.08116907963213)
-  ]
-  
   noStroke();
   canvas = createCanvas(ww, hh);
-  //canvas.mouseClicked(clickOnSave)
-
-  left_x = Math.round(ww * -0.5)
-  right_x = Math.round(ww * 1.5)
-  top_y = Math.round(hh * -0.5)
-  bottom_y = Math.round(hh * 1.5) 
-  resolution = Math.round(ww * 0.01) 
-  num_columns = Math.round((right_x - left_x) / resolution)
-  num_rows = Math.round((bottom_y - top_y) / resolution)
+  left_x = Math.round(ww * -0.5);
+  right_x = Math.round(ww * 1.5);
+  top_y = Math.round(hh * -0.5);
+  bottom_y = Math.round(hh * 1.5); 
+  resolution = Math.round(ww * 0.01); 
+  num_columns = Math.round((right_x - left_x) / resolution);
+  num_rows = Math.round((bottom_y - top_y) / resolution);
   
   grid = new Array(num_columns);
   for (var i=0; i<num_columns; i++)
     grid[i] = new Array(num_rows);
-  
-  // background(191, 185, 185);
-  // background(random(0,150) * random(0,1), random(0,150), random(0,150));
+
   background(255);
   
   for (var i=0; i<5; i++) {
-    drawCustomShape(createStretchedPentagon(pp[i][0], pp[i][1]), [random(0,150), random(0,150), random(0,150), 10])
+    drawCustomShape(createStretchedPentagon(pp[i][0], pp[i][1]), [random(0,150), random(0,150), random(0,150), 10]);
   }
   filter(BLUR, 3);
+
   beginShape();
   for (var column=0; column<num_columns; column++) {
     for (var row=0; row<num_rows; row++) {
-      // angle = (row / parseFloat(num_rows)) * Math.PI
-      // Processing's noise() works best when the step between
-      // points is approximately 0.005, so scale down to that
-      var scaled_x = parseFloat(column) * 0.01
-      var scaled_y = parseFloat(row) * 0.01
-      // get our noise value, between 0.0 and 1.0
-      var noise_val = noise(scaled_x, scaled_y)
-      // translate the noise value to an angle (betwen 0 and 2 * PI)
+      var scaled_x = parseFloat(column) * 0.01;
+      var scaled_y = parseFloat(row) * 0.01;
+      var noise_val = noise(scaled_x, scaled_y);
       angle = map(noise_val, 0.0, 1.0, 0.0, TWO_PI) + radians(100);
-      grid[column][row] = angle
+      grid[column][row] = angle;
     }
   }
-  // var ec = color(random(70,200) * random(0,1), random(0,250), random(0,250));
   var ec = color(255,255,255);
-
   ec.setAlpha(1);
   fill(ec);
   for (var i = 0; i < 10; i++) {
     var x = random(0, ww / 2);
     var y = random(0, hh / 2);
-    myFlowField(x, y, num_steps) ;
+    myFlowField(x, y, num_steps);
   }
   for (var i = 0; i < 10; i++) {
     var x = random(ww / 2, ww);
     var y = random(0, hh / 2);
-    myFlowField(x, y, num_steps) ;
+    myFlowField(x, y, num_steps);
   }
-
   endShape();
+
   tint(255,180);
   translate(ww / 2, hh / 2);
   rotate(PI / 180 * random(0,360));
   imageMode(CENTER);
   image(img, 0, 0, ww*1.5, hh*1.25);
   
-
-  //frameRate(0.1)
+  //frameRate(0.1);
 }
 
 function draw() {
-  // background(127);
   noStroke();
   let borderColor = color(191, 185, 185);
   fill(borderColor);
@@ -169,14 +111,13 @@ function myFlowField(x, y, num_steps){
   for (var n = 0; n < num_steps; n++) {
     var x_offset = x - left_x;
     var y_offset = y - top_y;
-    
     var column_index = Math.round(x_offset / resolution);
     var row_index = Math.round(y_offset / resolution);
     if (column_index >= num_columns)
       column_index = num_columns - 1;
     if (row_index >= num_rows)
       row_index = num_rows - 1;
-    // NOTE: normally you want to check the bounds here
+    
     var grid_angle = grid[column_index][row_index];
     var x_step = Math.abs(step_length * cos(grid_angle));
     var y_step = Math.abs(step_length * sin(grid_angle));
@@ -187,19 +128,17 @@ function myFlowField(x, y, num_steps){
     // pop();
     x = x + x_step;
     y = y + y_step;    
-    
   }
 }
 
 function drawCustomShape(shapeArchetype, color) {
   for (let j = 0; j < layers; j += 1) {
-    let shape = polygon(shapeArchetype, 1)
-    fill(color)
+    let shape = polygon(shapeArchetype, 1);
+    fill(color);
     beginShape();
     for (let i of shape) {
-      vertex(i.x, i.y)
+      vertex(i.x, i.y);
     }
-    
     //blendMode('darken');
     let pxmax = getXmax(shape);
     let pymax = getYmax(shape);
@@ -207,11 +146,9 @@ function drawCustomShape(shapeArchetype, color) {
     for (let k = 0; k < 100; k += 1) {
       let px = random(0, pxmax);
       let py = random(0, pymax);
-      let wh = Math.abs(randomGaussian(pxmax, 0.03) - randomGaussian(pxmax, 0.02)) * 1000;
-      
+      let wh = Math.abs(randomGaussian(pxmax, 0.03) - randomGaussian(pxmax, 0.02)) * 1000; 
       ellipse(px, py, wh, wh);  
     }
-    
     endShape(CLOSE);
   }
 }
@@ -236,34 +173,88 @@ function getYmax(shape) {
 
 function polygon(currentShape, dep) {
   if (dep >= 7) {
-    return currentShape
+    return currentShape;
   } else {
-    const nextShape = []
+    const nextShape = [];
     for (let i in currentShape) {
-      nextShape.push(currentShape[i])
-      let next = int(i) + 1
+      nextShape.push(currentShape[i]);
+      let next = int(i) + 1;
       try {
-        const midVector = createVector( (currentShape[next].x + currentShape[i].x) / 2
-          , (currentShape[next].y + currentShape[i].y) / 2)
-        const dx = randomGaussian(0, st_deviation)
-        const dy = randomGaussian(0, st_deviation)
-        midVector.add(dx, dy)
-        nextShape.push(midVector)
+        const midVector = createVector((currentShape[next].x + currentShape[i].x) / 2, (currentShape[next].y + currentShape[i].y) / 2);
+        const dx = randomGaussian(0, st_deviation);
+        const dy = randomGaussian(0, st_deviation);
+        midVector.add(dx, dy);
+        nextShape.push(midVector);
       } catch(e) {
         // Handle end of array
-        const midVector = createVector( (currentShape[0].x + currentShape[i].x) / 2
-          , (currentShape[0].y + currentShape[i].y) / 2)
-        const dx = randomGaussian(0, st_deviation)
-        const dy = randomGaussian(0, st_deviation)
-        midVector.add(dx, dy)
-        nextShape.push(midVector)
+        const midVector = createVector((currentShape[0].x + currentShape[i].x) / 2, (currentShape[0].y + currentShape[i].y) / 2);
+        const dx = randomGaussian(0, st_deviation);
+        const dy = randomGaussian(0, st_deviation);
+        midVector.add(dx, dy);
+        nextShape.push(midVector);
       }
     }
-    return polygon(nextShape, dep + 1)
-
+    return polygon(nextShape, dep + 1);
   }
 }
 
-function clickOnSave() {
-  saveCanvas();
+class Random {
+  constructor(seed) {
+      this.seed = seed;
+      this.originalSeed = seed;
+  }
+  randDec() {
+      this.seed ^= this.seed << 13;
+      this.seed ^= this.seed >> 17;
+      this.seed ^= this.seed << 5;
+      return ((this.seed < 0 ? ~this.seed + 1 : this.seed) % 1000) / 1000;
+  }
+  randNum(a, b) {
+      return a+(b-a)*this.randDec();
+  }
+  randInt(a, b) {
+      return Math.floor(this.randNum(a, b+1));
+  }
+
+  reset() {
+      this.seed = this.originalSeed;
+  }
 }
+
+function random_hash() {
+  let x = "0123456789abcdef", hash = '0x';
+  for (let i = 64; i > 0; --i) {
+    hash += x[Math.floor(Math.random()*x.length)];
+  }
+  return hash;
+}
+
+tokenData = {
+  //hash: "0x11ac16678959949c12d5410212301960fc496813cbc3495bf77aeed738579738", 
+  hash: random_hash(),
+  tokenId: "123000456"
+};
+
+function windowResized() {
+  hh = window.innerHeight;
+  if (hh > shh)
+    hh = shh;
+  ww = hh*ratio;
+  const dim = Math.min(ww, hh);
+  M = dim / defaultSize;
+  console.log(`M: ${M}`);
+  R.reset();
+  resizeCanvas(ww, hh);
+}
+
+const defaultSize = 1000;
+const ratio = 0.83;
+let hh = shh;
+let ww = sww;
+let dim = Math.min(ww, hh);
+let M = dim / defaultSize;
+// console.log(`M: ${M}`);
+// console.log(`Hash: ${tokenData.hash}`);
+const seed = parseInt(tokenData.hash.slice(0, 16), 16);
+// console.log(`Seed: ${seed}`);
+const R = new Random(seed);
